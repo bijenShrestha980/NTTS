078bd124f278249455d912fbc99c48cd5e38c590
bijenShrestha980 <s.bijen93@gmail.com>
2022-03-21 14:12:43 +0545

    preloader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+(function ($) {
+  ("use strict");
+  //preloader
+  $(window).on("load", function () {
+    $("#loader").delay(1000).fadeOut(500);
+  });
+})(jQuery);
 $(document).ready(function () {
   // Mobile Nav
   $("#mobile-btn").click(function () {
